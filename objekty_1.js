fcdeaff1e41a8e7d71a0fa225a2662c2b0160bff
KycIve <68947945+KycIve@users.noreply.github.com>
2022-10-06 13:04:10 +0200

    Add files via upload

---
+++ objekty_1.js
@@ -0,0 +1,25 @@
+
+const apartment = {
+	type: 'rent',
+	disposition: '3+1',
+	area: {
+		floorage: 100,
+		balcony: 2,
+		units: 'sqm',
+	},
+	city: 'Prague',
+	district: 'Old Town',
+	price: {
+		rent: 28000,
+		fees: {
+			water: 1000,
+			energy: 2500,
+			services: 560,
+		},
+		currency: 'czk',
+	},
+	ownership: 'personal',
+	condition: 'renovated',
+	status: 'free',
+	floor: 3,
+}
